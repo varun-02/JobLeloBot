67a831e977beffb4ebf20a4811044e10c1984924
Varun Aggarwal <96365773+varun-02@users.noreply.github.com>
2025-08-28 19:34:47 +0530

    Update job_bot.py

--- job_bot.py
+++ job_bot.py
@@ -10,6 +10,11 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 RAPID_API_KEY = os.getenv("RAPID_API_KEY")
 
+print("BOT_TOKEN:", bool(BOT_TOKEN))
+print("CHAT_ID:", CHAT_ID)
+print("RAPID_API_KEY:", bool(RAPID_API_KEY))
+
+
 # ===============================
 # 🔹 Fetch Jobs from LinkedIn API
 # ===============================
